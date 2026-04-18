ed25fcda5b5b498716873da77fb740e548d86900
Saif Kayyali <saifkayyali2011@gmail.com>
2026-04-18 09:57:14 +0300

    Added health endpoint

--- website/main.py
+++ website/main.py
@@ -135,6 +135,10 @@ def sitemap():
     urls = [{'loc': 'https://photonvhealth.onrender.com', 'lastmod': datetime.now().date()}]
     return Response(render_template('sitemap.xml', urls=urls), mimetype='application/xml')
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host = "0.0.0.0", port = port)
